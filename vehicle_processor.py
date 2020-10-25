db467b44e4be7cb4b0b10aaa936b2054bb460ff8
sfagade <sfagade@gmail.com>
2020-10-25 15:15:20 +0100

    Added proper readme info added implementation to save record in database

--- vehicle_processor.py
+++ vehicle_processor.py
@@ -5,6 +5,7 @@ import pandas
 import re
 import time
 from app_util import fetch_vehicle_make, load_properties, fetch_vehicle_model
+from data_connection import check_record_exist, save_new_record
 
 
 def start_processing():
@@ -12,7 +13,7 @@ def start_processing():
 
     site_url = prop_json['sites']['auto']
     records = []
-
+    created_at = time.strftime("%m/%d/%Y %H:%M:%S", time.localtime())
     for page in range(1, 10, 1):
         request = requests.get(site_url,
                                headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux '
@@ -23,9 +24,11 @@ def start_processing():
         main_table = soup.find("table", {"id": None})
         table_rows = main_table.find_all("tr")
 
+
         for row in table_rows:
             link = row.find_all("a")
             main_link = link[1]
+            ref_link = link[1].get("href")
 
             price = '0'
 
@@ -56,20 +59,30 @@ def start_processing():
                     price = price_list[0]
             if price == '0' or price == '.':
                 # find every amount that ends with m indicating million
-                price_list = re.search(r'\d{1,4}[m]', main_link.text.replace('matic', '').replace('month',''))
+                price_list = re.search(r'\d{1,4}[m]', main_link.text.replace('matic', '').replace('month', ''))
                 if price_list:
                     print("trying to pick: ", price_list)
                     price = price_list[0].replace('m', '')
 
+            data_row = {}
+
             if link[0].get("name") and identified_make and year:
-                data_row = {"record_id": link[0].get("name"), "make": identified_make, "model": identified_model,
-                            "year": year, "description": main_link.text.lower(), "price": price,
-                            "phone_number": phone_number}
+                views_tag = row.find("span", {"class": "s"}).find_all("b")[2]
+                post_by = row.find("span", {"class": "s"}).find("b").find("a").text
+                if not check_record_exist(link[0].get("name")):
+                    data_row = {"record_id": link[0].get("name"), "make": identified_make, "model": identified_model,
+                                "year": year, "price": price, "description": main_link.text.lower(), "url": ref_link,
+                                "views": views_tag.text, "posted_by": post_by, "phone_number": phone_number,
+                                "created_at": created_at}
+                    save_new_record(data_row)
+                else:
+                    print("Record exist in our database")
+
                 records.append(data_row)
             # ensures we will be navigating to the next page on our next iteration
             site_url = prop_json['sites']['auto'] + "/" + str(page)
 
-    date_now = time.strftime("%m_%d_%Y_%H%M%S", time.localtime())
     data_frame = pandas.DataFrame(records)
-    # print(data_frame)
+    date_now = time.strftime("%m_%d_%Y_%H%M%S", time.localtime())
     data_frame.to_csv("~/Documents/scraped_data/scrape_" + date_now + "_output.csv")
+    print("Total records: ", len(records))
